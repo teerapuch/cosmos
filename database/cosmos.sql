5cf6b1cb55435124a15eb7adef5a3f06f2bbc829
teerapuch <teerapuch@hotmail.com>
2018-11-15 07:32:56 +0700

    Update Database

--- database/cosmos.sql
+++ database/cosmos.sql
@@ -1,20 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
--- http://www.phpmyadmin.net
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 19, 2018 at 04:18 PM
--- Server version: 5.6.24
--- PHP Version: 5.6.8
+-- Generation Time: Nov 15, 2018 at 01:32 AM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `cosmos`
@@ -26,26 +28,26 @@ SET time_zone = "+00:00";
 -- Table structure for table `user_tbl`
 --
 
-CREATE TABLE IF NOT EXISTS `user_tbl` (
+CREATE TABLE `user_tbl` (
   `id` int(11) NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `firstname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `lastname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `avatar` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `status` tinyint(1) NOT NULL,
-  `lavel` tinyint(1) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT 'สำหรับเปิดให้ทำงานเป็น 1 ไม่ให้ทำงานเป็น 2',
+  `level` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'user = 0, admin = 1',
   `updatedate` datetime NOT NULL,
   `createdate` datetime NOT NULL,
   `deleted` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `user_tbl`
 --
 
-INSERT INTO `user_tbl` (`id`, `email`, `password`, `firstname`, `lastname`, `avatar`, `status`, `lavel`, `updatedate`, `createdate`, `deleted`) VALUES
-(1, 'admin@mail.com', '$2y$12$uV42PUQ4vR81kF3Zj5rgBebAccntza9oKUDxI.IhhHABsgOokp7sG', '', '', '', 0, 0, '2018-01-19 15:56:52', '2018-01-19 15:56:52', 0);
+INSERT INTO `user_tbl` (`id`, `email`, `password`, `firstname`, `lastname`, `avatar`, `status`, `level`, `updatedate`, `createdate`, `deleted`) VALUES
+(1, 'admin@mail.com', '$2y$12$uV42PUQ4vR81kF3Zj5rgBebAccntza9oKUDxI.IhhHABsgOokp7sG', '', '', '', 1, 1, '2018-01-19 15:56:52', '2018-01-19 15:56:52', 0);
 
 --
 -- Indexes for dumped tables
@@ -65,7 +67,9 @@ ALTER TABLE `user_tbl`
 -- AUTO_INCREMENT for table `user_tbl`
 --
 ALTER TABLE `user_tbl`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
